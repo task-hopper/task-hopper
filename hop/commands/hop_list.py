c85f1ca80d40e16ba858e6dfc6b2139951c47c26
Michael Buckley <michael.buckley@cordatahealth.com>
2020-03-10 18:08:53 -0400

    add `hop list` command

---
+++ hop/commands/hop_list.py
@@ -0,0 +1,14 @@
+from ._hop_command import HopCommand
+from tasks.list_projects import ListProjects
+
+
+class List(HopCommand):
+    def __init__(self):
+        self.alias = 'list'
+
+    def setup_command(self, subparsers):
+        subparsers.add_parser(
+            'list', help='lists configured projects')
+
+    def process_command(self, parsed_args):
+        ListProjects.stage()
---
+++ hop/tasks/list_projects.py
@@ -0,0 +1,20 @@
+from composer import composer
+from config_handler import configs
+from tasks._task import Task
+
+
+class ListProjects(Task):
+    @classmethod
+    def stage(cls):
+        all_projects = configs.configs['projects']
+        current_project = configs.current_project()
+
+        project_list = [name for name, config in all_projects.items()]
+        sorted_list = sorted(project_list)
+        colored_list = [f'{"=>" if item == current_project else "  "} \e[32m{item}\e[0m' for item in sorted_list]
+
+        message = colored_list
+        message.append('')
+        message.append('# => - current')
+
+        composer.add('message', message)
