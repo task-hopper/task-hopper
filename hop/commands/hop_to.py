7450a3bf8d4b5a497b2c9fa45ff9942a5885f07c
Josh Crank <joshuatcrank@gmail.com>
2020-03-11 09:54:13 -0400

    remove ability to hop to directories, and limit to only projects

--- hop/commands/hop_to.py
+++ hop/commands/hop_to.py
@@ -11,14 +11,10 @@ class To(HopCommand):
         to_parser = subparsers.add_parser(
             'to', help='changes directory')
         to_parser.add_argument(
-            'destination', help='specify project/directory to switch to')
+            'destination', help='specify project')
 
     def process_command(self, parsed_args):
-        is_dir = exists(apath(parsed_args.destination))
-        if is_dir:
-            self.push_task('CD', directory=apath(parsed_args.destination))
-        else:
-            # prompt cd
-            self.push_task('CD', project_name=parsed_args.destination)
-            # export configured environment variables if autoload is enabled
-            self.push_task('ChangeEnv', project_name=parsed_args.destination, env='autoload')
+        # prompt cd
+        self.push_task('CD', project_name=parsed_args.destination)
+        # export configured environment variables if autoload is enabled
+        self.push_task('ChangeEnv', project_name=parsed_args.destination, env='autoload')
--- hop/tasks/cd.py
+++ hop/tasks/cd.py
@@ -3,11 +3,7 @@ from helpers.utils import apath
 from tasks._task import Task
 
 class CD(Task):
-    def stage(self, project_name=None, directory=None):
-        if project_name:
-            project_configs = configs.project_configs(project_name)
-            project_path = apath(project_configs.get("path"))
-            self.composer.add('task', f'cd {project_configs.get("path")}')
-        else:
-            abs_directory = apath(directory)
-            self.composer.add('task', f'cd {abs_directory}')
+    def stage(self, project_name=None):
+        project_configs = configs.project_configs(project_name)
+        project_path = apath(project_configs.get("path"))
+        self.composer.add('task', f'cd {project_configs.get("path")}')
