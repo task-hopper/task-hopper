7b23b2906089a76d95a01061525fd26a7ec66c13
Michael Buckley <michael.buckley@cordatahealth.com>
2020-03-14 13:41:47 -0400

    add --no-color option to `hop list`

--- hop/commands/hop_list.py
+++ hop/commands/hop_list.py
@@ -7,8 +7,12 @@ class List(HopCommand):
         self.alias = 'list'
 
     def setup_command(self, subparsers):
-        subparsers.add_parser(
+        list_parser = subparsers.add_parser(
             'list', help='lists configured projects')
 
+        list_parser.add_argument(
+            '--no-color', help='do not use colors in output',
+            action='store_const', const=True, dest='no_color')
+
     def process_command(self, parsed_args):
-        self.push_task('ListProjects')
+        self.push_task('ListProjects', no_color=parsed_args.no_color)
--- hop/tasks/list_projects.py
+++ hop/tasks/list_projects.py
@@ -3,13 +3,18 @@ from tasks._task import Task
 
 
 class ListProjects(Task):
-    def stage(self):
-        all_projects = configs.configs['projects']
+    def stage(self, no_color=False):
+        all_projects = configs.configs.get('projects')
         current_project = configs.current_project()
 
         project_list = [name for name, config in all_projects.items()]
         sorted_list = sorted(project_list)
-        colored_list = [f'{"=>" if item == current_project else "  "} \e[32m{item}\e[0m' for item in sorted_list]
+
+        use_color = not no_color
+        begin_color = '\e[32m'
+        end_color = '\e[0m'
+
+        colored_list = [f'{"=>" if item == current_project else "  "} {begin_color if use_color else ""}{item}{end_color if use_color else ""}' for item in sorted_list]
 
         message = colored_list
         message.append('')
