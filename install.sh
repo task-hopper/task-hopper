c64d00e882814a4651b31c6d9f3bdcdc41c4109a
Michael Buckley <iammikebuckley@gmail.com>
2022-03-03 23:06:36 -0500

    continue on install script; prepare for testing

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 # TODO
-# - actually download hop repo somehow
+# - probably set up pipenv and use that
 # - add `hop upgrade` command to fetch latest version of hop
 # - preserve existing hop config files
 # - colorize output from install script
@@ -17,24 +17,30 @@ SHELL_RC_FILE=$HOME/.bash_profile
 
 REQUIRED_PYTHON_PACKAGES=("pyyaml" "jinja2")
 
+# ensure python3 is installed
 if [ ! -x "$(command -v python3)" ]; then
   echo "TaskHopper requires Python 3 to be installed."
   exit 1
 fi
 
+# ensure pip3 is installed
 if [ ! -x "$(command -v pip3)" ]; then
   echo "TaskHopper requires Pip 3 to be installed."
   exit 1
 fi
 
+# install requirements
 for req in "${REQUIRED_PYTHON_PACKAGES[@]}"; do
   echo "Installing $req..."
   pip3 install $req --disable-pip-version-check
 done
 
-# TODO figure out how to do this without git clone
-# git clone git@github.com:task-hopper/task-hopper $INSTALL_DIR
+# download task-hopper repo
+curl --location --remote-header-name --remote-name https://github.com/task-hopper/task-hopper/archive/master.zip --output $INSTALL_DIR.zip
+unzip $INSTALL_DIR.zip -d $INSTALL_DIR
+rm -f $INSTALL_DIR.zip
 
+# add necessary environment variables / setup to user's shell configuration (.bashrc etc)
 EXPORT_COMMAND="export HOP_DIR=\"$INSTALL_DIR\""
 SOURCE_COMMAND="source \$HOP_DIR/hop/ext/hop.sh"
 
@@ -52,6 +58,8 @@ else
   echo $SOURCE_COMMAND >> $SHELL_RC_FILE
 fi
 
+# TODO figure out why this shows the file as having a syntax error on mac
+# source hop.sh to make hop immediately available in the current terminal session
 source $HOP_DIR/hop/ext/hop.sh
 
 echo "TaskHopper installation is complete! Run \`hop\` now to see available commands."
